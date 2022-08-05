95c453a6c06d81610ac123819a6991d8abaf547c
Victor Arnaud <victorhad@gmail.com>
2022-08-05 19:43:11 -0300

    Start hot-reload rest api

---
+++ rest_api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const http = require('http');
+
+if (process.env.NODE_ENV !== 'production') {
+  const chokidar = require('chokidar');
+  const watcher = chokidar.watch('./src', {
+    persistent: false,
+    ignoreInitial: true
+  });
+
+  watcher.on('ready', function() {
+    watcher.on('all', function() {
+      console.log("Limpando o cache dos modulos da pasta src e atualizando o servidor!");
+      Object.keys(require.cache).forEach(function(id) {
+        const localId = id.substring(process.cwd().length);
+        if (!localId.match(/^\/src\//)) return;
+        delete require.cache[id];
+      });
+    });
+  });
+}
+
+app.use((req, res, next) => require("./src/routes")(req, res, next));
+const server = http.createServer(app);
+server.listen(port, 'localhost', function(error) {
+  if (error) throw error;
+  const addr = server.address();
+  console.log('Rodando em http://%s:%d', addr.address, addr.port);
+});
